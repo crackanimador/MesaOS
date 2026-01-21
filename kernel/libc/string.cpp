86ce47a9489ff060d0bb19b6cbc33c43fb3b3f0e
crackanimador <104255528+crackanimador@users.noreply.github.com>
2026-01-21 22:40:17 +0100

    Add files via upload

--- kernel/libc/string.cpp
+++ kernel/libc/string.cpp
@@ -91,6 +91,24 @@ char* strchr(const char* s, int c) {
     return (char*)s;
 }
 
+void* memmove(void* dest, const void* src, size_t n) {
+    unsigned char* d = (unsigned char*)dest;
+    const unsigned char* s = (const unsigned char*)src;
+    
+    if (d < s) {
+        // Copy forward
+        for (size_t i = 0; i < n; i++) {
+            d[i] = s[i];
+        }
+    } else if (d > s) {
+        // Copy backward
+        for (size_t i = n; i > 0; i--) {
+            d[i-1] = s[i-1];
+        }
+    }
+    return dest;
+}
+
 char* itoa(int value, char* str, int base) {
     char *rc;
     char *ptr;
